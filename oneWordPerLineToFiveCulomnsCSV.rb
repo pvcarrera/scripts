080f26224d39470ccb9e65dd5da842cd14705421
pvcarrera <pvicente@agil-az.com>
2011-10-05 18:08:36 +0200

    Change , to tabs and 5 to 6 columns

--- oneWordPerLineToFiveCulomnsCSV.rb
+++ oneWordPerLineToFiveCulomnsCSV.rb
@@ -1,12 +1,14 @@
 # encoding: UTF-8
 #El fichero de entrada tiene una palabra por línea
-#Lo convierto a lineas de 5 columas en formato csv
+#Lo convierto a lineas de 6 columas en formato csv
 #
 lines = File.open(ARGV[0]).readlines
 array = []
 lines.each do |line|
 	array << line.gsub!(/[\n]+/, "")
 end
-array.each_slice(6) do |five|
-	File.open(ARGV[1], "a"){ |f| f << "#{five.to_s.gsub!(/[\[\]]/,"")}\n" }
+array.each_slice(7) do |five|
+	cleanLine = five.to_s.gsub!(/[\[\]]/,"")
+        cleanLine = cleanLine.gsub("\",", "\"\t")	
+	File.open(ARGV[1], "a"){ |f| f << "#{cleanLine}\n"}
 end
